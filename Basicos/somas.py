dd5d0d69c17c679500f617c28de0513d00551a7d
Lucas Agostinho Fernandes <36972783+LucasAgosti@users.noreply.github.com>
2018-09-19 11:53:29 -0300

    Python

---
+++ Basicos/somas.py
@@ -0,0 +1,19 @@
+# Soma de 0 a 99
+s1 = 0
+for x in range(1, 11):
+    s1 += x
+    print (s1)
+#Imprimindo todas as letras de uma palavra
+linguagens = ['C', 'C++', 'Java', 'Python']
+print("*-----------------------*")
+for letters in linguagens:
+        print("\tOrdem: ", letters)
+
+print("*-----------------------*")
+print("\nUsando while:")
+s2 = 0
+z = 1
+while z < 10:
+    s2 += z
+    z += 1
+    print (s2)
