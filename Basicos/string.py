9b8575478e21c54e837bad26a51efaf26ef7c261
Lucas Agostinho Fernandes <36972783+LucasAgosti@users.noreply.github.com>
2018-09-19 11:53:45 -0300

    Python

---
+++ Basicos/string.py
@@ -0,0 +1,13 @@
+# Treino de strings
+# coding: utf-8
+str = "Deus do céu"
+print("Meu " + str + " Berg")
+
+print("O tamanho da string " + str + " é: ", (str, len(str)))
+print()
+for ch in str:
+    print(ch)
+
+str2 = "noooooooooossa mano "
+print(3 * str2)
+print("Python"[::-1])
